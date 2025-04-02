8e706f0af455b22f693a7b61ca97fa918d4b5642
Param Khodiyar <paramkhodiya1008@gmail.com>
2025-04-02 15:46:59 +0530

    initialised the repo and created the navbar with search

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar" aria-label="Main Navigation">
+                <span>
+                    <img src="/logo.png" alt="Airbnb Icon"></img>
+                    <h2 className="logo">AirBnb</h2>
+                </span>
+                <div className="pages">
+                    <ul>
+                        <li><a href="#">Homes</a></li>
+                        <li><a href="#">Experience</a></li>
+                    </ul>
+                </div>
+                <div className="searchContainer">
+                    <div className="searchbox">
+                        <input className="homes" placeholder="Search..."></input>
+                        <input className="dates" placeholder="Dates..."></input>
+                        <input className="guests" placeholder="No. Of Guests..."></input>
+                        <button class="btn-search"><i class="fas fa-search">Hello</i></button>
+                    </div>
+                </div>
+            </nav>
+        </>
+        
+    );
+}
+
+export default Navbar;
