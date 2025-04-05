5ccfacc6afc5c124c3fa6a8cfe7a7712cabb8f04
Param Khodiyar <paramkhodiya1008@gmail.com>
2025-04-05 15:25:25 +0530

    Added badge

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import "./card.css"
 import hotels from "./hotels.json";
+import { IoHeartSharp } from "react-icons/io5";
 
-function Cards(){
+function Cards() {
 
     return (
         <div className="cardcontainer">
             {hotels.listings.map((hotel, index) => (
-                <div key={index} className="card">
-                    <img src={hotel.image_src} alt={hotel.hotel_name} />
-                    <h3>{hotel.hotel_name}</h3>
-                    <p>Guest Favorite: {hotel.guest_favorite ? "Yes" : "No"}</p>
-                    <p>{hotel.date_available}</p>
-                    <p className="price"><span>₹{hotel.price}</span> per night</p>
-                </div>
+               <div className="card-wrapper">
+               <div key={index} className="card">
+                 <div className="imgbdgeholder">
+                   <img
+                     src={hotel.image_src}
+                     alt={hotel.hotel_name}
+                     className="hotel-image"
+                   />
+                   <div className="guestfav">
+                        Guest Fav : 
+                     <IoHeartSharp
+                       className="heart-icon"
+                       style={{
+                         color: hotel.guest_favorite ? "#ff385c" : "white",
+                       }}
+                     />
+                   </div>
+                 </div>
+                 <h3>{hotel.hotel_name}</h3>
+                 <p>{hotel.date_available}</p>
+                 <p className="price">
+                   <span>₹{hotel.price}</span> per night
+                 </p>
+               </div>
+             </div>
+             
             ))}
         </div>
     );
